a244f8370e93b9d3d13bdc7bef232ad096b00e9d
sebitas23045 <sebita.23045@gmail.com>
2025-04-09 14:37:05 -0500

    subiendo repo

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import setupFormHandler from "./js/modules/formHandler.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupFormHandler(); // configurar el listener del formulario
-});
