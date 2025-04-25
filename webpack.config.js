c103940df764bc06e69d3a1b92d6a1c3d6123a6d
Sebitas23045 <sebita.23045@gmail.com>
2025-04-25 16:45:24 -0500

    arreglando errores

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/main.js', // 🛠️ Corrigiendo la entrada
+  entry: './src/main.js', // ← correcto!
   output: {
-    filename: 'index.bundle.js',
+    filename: '[name].bundle.js', // index.bundle.js
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // 🧹 limpia la carpeta dist antes de cada build
+    clean: true,
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -16,14 +16,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html', // correcto
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {
@@ -34,7 +29,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/images/[name][ext]',
+          filename: 'assets/images/[name][ext]', // perfecto
         },
       },
     ],
