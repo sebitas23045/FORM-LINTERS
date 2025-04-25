5c19359f0da599f589de135434315f909262d72c
Sebitas23045 <sebita.23045@gmail.com>
2025-04-25 16:57:36 -0500

    corrigiendo errores

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/main.js',
+  entry: './src/main.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
